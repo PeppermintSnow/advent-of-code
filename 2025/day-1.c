5b8311f87b6286b8c41b55d0af790297a313d698
PeppermintSnow <168705921+PeppermintSnow@users.noreply.github.com>
2025-12-04 21:31:21 +0800

    feat(2025): add part 2 solution for day 1

--- 2025/day-1.c
+++ 2025/day-1.c
@@ -5,7 +5,7 @@
 // The dial initially points at 50
 #define DIAL_START 50
 // Only read 5 bytes per instruction + 1 for null terminator
-#define INSTRUCTION_LENGTH 6 
+#define INSTRUCTION_LENGTH 6
 
 typedef struct Node {
     char* instruction;
@@ -16,16 +16,16 @@ typedef struct Node {
  * Rotates the dial by the given value
  *
  * @param dial Value which the dial points to
+ * @param zeroDials Number of times the dial HITS zero
  * @param value Rotation value which follows the form L/R0-99 e.g., L5, R99
  * @return New dial value
  */
-int rotateDial(int dial, char* instruction);
+int rotateDial(int dial, int* timesDialHitsZero, char* instruction);
 
 int main(int argc, char** argv) {
     int dial = DIAL_START;
     char** instructions;
     FILE* fptr;
-    Node* head = malloc(sizeof(Node));
 
     if (argv[1]) {
         fptr = fopen(argv[1], "r");
@@ -38,61 +38,66 @@ int main(int argc, char** argv) {
         exit(1);
     }
 
-    Node* currNode = head;
+    Node* head = NULL;
+    Node* tail = NULL;
+
     char buffer[INSTRUCTION_LENGTH];
     while (fgets(buffer, INSTRUCTION_LENGTH, fptr)) {
         buffer[strcspn(buffer, "\n")] = '\0';
 
-        currNode->instruction = malloc(INSTRUCTION_LENGTH);
-        strncpy(currNode->instruction, buffer, INSTRUCTION_LENGTH);
-
         Node* tempNode = malloc(sizeof(Node));
-        tempNode->instruction = NULL;
+
+        tempNode->instruction = malloc(INSTRUCTION_LENGTH);
+        strncpy(tempNode->instruction, buffer, INSTRUCTION_LENGTH);
         tempNode->next = NULL;
 
-        currNode->next = tempNode;
-        currNode = currNode->next;
+        if (!head) {
+            head = tempNode;
+            tail = tempNode;
+        } else {
+            tail->next = tempNode;
+            tail = tempNode;
+        }
     }
 
-    // Password is the number of times the dial hits 0
-    int zeroDials = 0;
-    currNode = head;
-    while(currNode->next != NULL) {
-        dial = rotateDial(dial, currNode->instruction);
-        if (dial == 0) zeroDials++;
-        currNode = currNode->next;
+    // Part 1 states that the number of times the dial STOPS at 0 is the password
+    // Part 2 states that the number of times the dial PASSES 0 is the password
+    int timesDialHitsZero = 0;
+    int timesDialStopsAtZero = 0;
+    tail = head;
+    while(tail != NULL) {
+        dial = rotateDial(dial, &timesDialHitsZero, tail->instruction);
+        if (dial == 0) timesDialStopsAtZero++;
+        tail = tail->next;
     }
 
-    currNode = head;
-    while(currNode->next != NULL) {
-        Node* temp = currNode;
-        currNode = currNode->next;
+    tail = head;
+    while(tail != NULL) {
+        Node* temp = tail;
+        tail = tail->next;
         free(temp->instruction);
         free(temp);
     }
 
-    printf("The password is %d!", zeroDials);
+    printf("The password is for part 1 is %d!\n", timesDialStopsAtZero);
+    printf("The password is for part 2 is %d!\n", timesDialHitsZero);
     return 0;
 }
 
-int rotateDial(int dial, char* instruction) {
+int rotateDial(int dial, int* timesDialHitsZero, char* instruction) {
     char direction = instruction[0];
-    if (direction != 'L' && direction != 'R') return -1;
+    if (direction != 'L' && direction != 'R') exit(1);
 
-    char buffer[INSTRUCTION_LENGTH];
-    strncpy(buffer, instruction + 1, INSTRUCTION_LENGTH);
-    int rotateValue = strtol(buffer, NULL, 10);
+    int rotateValue = strtol(instruction + 1, NULL, 10);
 
-    // Right clockwise, left counter-clockwise
-    dial = direction == 'L' ? dial - rotateValue : dial + rotateValue;
+    // Brute force method cuz ts makes me wanna cry
+    int step = (direction == 'L') ? -1 : 1;
+    for (int i = 0; i < rotateValue; i++) {
+        dial += step;
+        if (dial < 0) dial = 99;
+        if (dial > 99) dial = 0;
 
-    // Dial can only be 0-99
-    while (direction == 'L' && dial < 0) {
-        dial += 100;
-    }
-
-    while (direction == 'R' && dial > 99) {
-        dial -= 100;
+        if (dial == 0) (*timesDialHitsZero)++;
     }
 
     return dial;
