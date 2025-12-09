eaa42d2e74a352b618c928c6ea1a46b127b319a8
PeppermintSnow <168705921+PeppermintSnow@users.noreply.github.com>
2025-12-09 22:19:24 +0800

    feat(2025): add solution for day 4 part 2

--- 2025/day-4.c
+++ 2025/day-4.c
@@ -5,7 +5,9 @@
 #define BUFFER_SIZE 256
 
 
+int evaluateGrid(char **grid, char **gridClone, size_t rows, size_t cols);
 int countRolls(int neighborsc, char *neighborsv);
+char **cloneGrid(char **src, size_t rows, size_t cols);
 
 int main(int argc, char **argv) {
     if (argc < 2) {
@@ -20,7 +22,7 @@ int main(int argc, char **argv) {
         exit(1);
     }
 
-    char **grid = malloc(2 * sizeof(char *));
+    char **grid = malloc(sizeof(char *));
     
     char buffer[BUFFER_SIZE];
     int lineIndex = 0;
@@ -48,17 +50,46 @@ int main(int argc, char **argv) {
         lineIndex++;
     }
 
-    int accessibleRolls = 0;
     size_t len = strlen(grid[0]);
-    for (int i = 0; i < lineIndex; i++) {
-        for (int j = 0; j < len; j++) {
+    char **gridClone = cloneGrid(grid, lineIndex, len);
+    int initialAccessibleRolls = 0;
+    int totalAccessibleRolls = 0;
+    int removableRolls = 0;
+    int iteration = 0;
+    do {
+        removableRolls = evaluateGrid(grid, gridClone, lineIndex, len);
+
+        for (int i = 0; i < lineIndex; i++) free(grid[i]);
+        free(grid);
+        grid = cloneGrid(gridClone, lineIndex, len);
+        
+        if (iteration == 0) initialAccessibleRolls = removableRolls;
+        totalAccessibleRolls += removableRolls;
+        iteration++;
+    } while (removableRolls > 0);
+
+    printf("Initial accesible rolls: %d\n", initialAccessibleRolls);
+    printf("Total accessible rolls: %d\n", totalAccessibleRolls);
+
+    for (int i = 0; i < lineIndex; i++) free(grid[i]);
+    for (int i = 0; i < lineIndex; i++) free(gridClone[i]);
+    free(grid);
+    free(gridClone);
+    fclose(fptr);
+    return 0;
+}
+
+int evaluateGrid(char **grid, char **gridClone, size_t rows, size_t cols) {
+    int accessibleRolls = 0;
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
             if (grid[i][j] != '@') {
                 printf(".");
                 continue;
             }
             int flag = 0;
             // General case
-            if (i > 0 && j > 0 && i < lineIndex - 1 && j < len - 1) {
+            if (i > 0 && j > 0 && i < rows - 1 && j < cols - 1) {
                 char neighbors[] = {
                     grid[i - 1][j - 1],     grid[i - 1][j],     grid[i - 1][j + 1],
                     grid[i][j - 1],     /*  grid[i][j],     */  grid[i][j + 1],
@@ -70,7 +101,7 @@ int main(int argc, char **argv) {
 
             // Edge cases
             // Top
-            if (i == 0 && j > 0 && j < len - 1) {
+            if (i == 0 && j > 0 && j < cols - 1) {
                 char neighbors[] = {
                 /*  grid[i - 1][j - 1],     grid[i - 1][j],     grid[i - 1][j + 1], */
                     grid[i][j - 1],     /*  grid[i][j],     */  grid[i][j + 1],
@@ -80,7 +111,7 @@ int main(int argc, char **argv) {
                 flag = countRolls(5, neighbors) < 4;
 
             // Right
-            } else if (i > 0 && i < lineIndex - 1 && j == len - 1) {
+            } else if (i > 0 && i < rows - 1 && j == cols - 1) {
                 char neighbors[] = {
                     grid[i - 1][j - 1],     grid[i - 1][j], /*  grid[i - 1][j + 1], */
                     grid[i][j - 1],     /*  grid[i][j],         grid[i][j + 1],     */
@@ -90,7 +121,7 @@ int main(int argc, char **argv) {
                 flag = countRolls(5, neighbors) < 4;
 
             // Bottom
-            } else if (i == lineIndex - 1 && j > 0 && j < len - 1) {
+            } else if (i == rows - 1 && j > 0 && j < cols - 1) {
                 char neighbors[] = {
                     grid[i - 1][j - 1],     grid[i - 1][j],     grid[i - 1][j + 1],
                     grid[i][j - 1],     /*  grid[i][j],     */  grid[i][j + 1],
@@ -100,7 +131,7 @@ int main(int argc, char **argv) {
                 flag = countRolls(5, neighbors) < 4;
 
             // Left
-            } else if (i > 0 && i < lineIndex - 1 && j == 0) {
+            } else if (i > 0 && i < rows - 1 && j == 0) {
                 char neighbors[] = {
                 /*  grid[i - 1][j - 1], */  grid[i - 1][j],     grid[i - 1][j + 1],
                 /*  grid[i][j - 1],         grid[i][j],     */  grid[i][j + 1],
@@ -120,7 +151,7 @@ int main(int argc, char **argv) {
                 flag = countRolls(3, neighbors) < 4;
 
             // Top-right
-            } else if (i == 0 && j == len - 1) {
+            } else if (i == 0 && j == cols - 1) {
                 char neighbors[] = {
                 /*  grid[i - 1][j - 1],     grid[i - 1][j],     grid[i - 1][j + 1], */
                     grid[i][j - 1],     /*  grid[i][j],         grid[i][j + 1],     */
@@ -130,7 +161,7 @@ int main(int argc, char **argv) {
                 flag = countRolls(3, neighbors) < 4;
 
             // Bottom-right
-            } else if (i == lineIndex - 1 && j == len - 1) {
+            } else if (i == rows - 1 && j == cols - 1) {
                 char neighbors[] = {
                     grid[i - 1][j - 1],     grid[i - 1][j], /*  grid[i - 1][j + 1], */
                     grid[i][j - 1],     /*  grid[i][j],         grid[i][j + 1],     */
@@ -140,7 +171,7 @@ int main(int argc, char **argv) {
                 flag = countRolls(3, neighbors) < 4;
             
             // Bottom-left
-            } else if (i == lineIndex - 1 && j == 0) {
+            } else if (i == rows - 1 && j == 0) {
                 char neighbors[] = {
                 /*  grid[i - 1][j - 1], */  grid[i - 1][j],     grid[i - 1][j + 1],
                 /*  grid[i][j - 1],         grid[i][j],     */  grid[i][j + 1],
@@ -151,17 +182,15 @@ int main(int argc, char **argv) {
             }
 
             printf("%c", flag ? 'x' : '@');
+            if (flag) {
+                gridClone[i][j] = '.';
+            }
             accessibleRolls += flag;
         }
         printf("\n");
     }
-
-    printf("\nAccessible rolls: %d", accessibleRolls);
-
-    for (int i = 0; i < lineIndex; i++) free(grid[i]);
-    free(grid);
-    fclose(fptr);
-    return 0;
+    printf("\n");
+    return accessibleRolls;
 }
 
 int countRolls(int neighborsc, char *neighborsv) {
@@ -170,4 +199,21 @@ int countRolls(int neighborsc, char *neighborsv) {
         if (neighborsv[i] == '@') numRolls++;
     }
     return numRolls;
+}
+
+char **cloneGrid(char **src, size_t rows, size_t cols) {
+    char **clonedGrid = malloc(rows * sizeof(char *));
+    if (!clonedGrid) {
+        printf("Failed to clone grid");
+        free(clonedGrid);
+        return NULL;
+    }
+
+    for (size_t i = 0; i < rows; i++) {
+        clonedGrid[i] = malloc(cols + 1);
+        memcpy(clonedGrid[i], src[i], rows);
+        clonedGrid[i][rows] = '\0';
+    }
+
+    return clonedGrid;
 }
